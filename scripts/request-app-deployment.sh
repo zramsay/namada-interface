e997e685b08b8c26c6fcc4dafdc37798269eedbe
zramsay <zach@bluecollarcoding.ca>
2025-01-29 17:59:34 -0500

    might not matter

--- scripts/request-app-deployment.sh
+++ scripts/request-app-deployment.sh
@@ -47,7 +47,7 @@ record:
   name: "$rcd_name@$rcd_app_version"
   application: "$CERC_REGISTRY_APP_LRN@$rcd_app_version"
   deployer: $DEPLOYER_LRN
-  dns: "$CERC_REGISTRY_DEPLOYMENT_HOSTNAME"
+  dns: $CERC_REGISTRY_DEPLOYMENT_HOSTNAME
   config:
     env:
       LACONIC_HOSTED_CONFIG_laconicd_chain_id: laconic-testnet-2
